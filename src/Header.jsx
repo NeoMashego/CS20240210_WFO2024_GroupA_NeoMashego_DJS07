5ab29bc3fc5109083f52992ee6c31655622d874b
NeoMashego <mashegonoj@gmail.com>
2024-11-14 04:41:55 +0200

    header file created and content added

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import trollFace from './assets/troll-face.png'
+
+function Header(){
+    return(
+        <header className="header">
+            <img className="header-img" src={trollFace} alt="troll face image"></img>
+            <h1 className="header-title">Meme Generator</h1>
+            <h4 className="header-project">React Course - Project 3</h4>
+        </header>
+    );
+}
+
+export default Header
