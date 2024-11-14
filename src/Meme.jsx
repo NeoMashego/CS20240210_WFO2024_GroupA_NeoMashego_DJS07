3211839c5459a8d52294829a5018e1db81b0e454
NeoMashego <mashegonoj@gmail.com>
2024-11-14 09:47:29 +0200

    getMemeImage function created

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,11 +1,25 @@
+import memesData from './assets/MemeData.js'
 
 function Meme(){
+    let url
+
+    function getMemeImage(){
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
+
     return(
         <main>
             <form className="form">
-                <input className="form-inputs" type="text" placeholder="Top text"></input>
-                <input className="form-inputs" type="text" placeholder="Bottom text"></input>
-                <button className="form-button">Get a new meme image </button>
+                <label>Top Text
+                    <input className="form-inputs" type="text" placeholder="Top text" />
+                </label>
+                <label>Bottom Text
+                    <input className="form-inputs" type="text" placeholder="Bottom text" />
+                </label>
+                <button className="form-button" onClick={getMemeImage}>Get a new meme image 🖼</button>
             </form>
         </main>
     );
